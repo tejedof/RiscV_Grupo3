b2562de1c3ce256876d56572425f2ae11345bb8f
tejedof <carlostejedofuster@gmail.com>
2022-11-15 12:21:25 +0100

    new file:   bubblesort.s 	new file:   bubblesort_hex.hex

---
+++ bubblesort.s
@@ -0,0 +1,57 @@
+#Cargamos la lista
+.data
+	n0: .byte 6
+	n1: .byte 9
+	n2: .byte 3
+	n3: .byte 1
+
+.text
+main:	add x2, x0, x0
+		add x3, x0, x0
+		addi x3, x3, 3
+		la t2, n3
+		lb t6, 0(t2)
+		la t2, n2
+		lb t5, 0(t2)
+		la t2, n1
+		lb t4, 0(t2)
+		la t2, n0
+		lb t3, 0(t2)
+loop:
+loop1:	
+		BLT t3, t4,loop2
+		add x1, t3, x0
+		add t3, t4, x0
+		add t4, x1, x0		
+loop2:
+		BLT t4, t5,loop3
+		add x1, t4, x0
+		add t4, t5, x0
+		add t5, x1, x0
+loop3:
+		BEQ x2, x3, guardar
+		addi x2, x2, 1
+		BLT t5, t6, loop1
+		add x1, t5, x0
+		add t5, t6, x0
+		add t6, x1, x0
+		j loop1
+
+#Necesitaremos:
+#Dos registros para llevar la cuenta en bucle
+#Registro auxiliar
+
+#Bucle for 1
+guardar:
+		la t2, n3
+		sb  0(t2), t6
+		la t2, n2
+		sb 0(t2),t5
+		la t2, n1
+		sb 0(t2),t4
+		la t2, n0
+		sb 0(t2),t3
+		
+#Bucle for 2
+
+finish:
