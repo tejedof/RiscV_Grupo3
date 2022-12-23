b1f572cc9dc47d68aced92ca06b3ef5e1bd84bdd
jovigob <joviedogoberna@gmail.com>
2022-12-23 17:32:41 +0100

    Correcciones.

--- GPIO.sv
+++ GPIO.sv
@@ -4,19 +4,17 @@ input [15:0] mem1_dout, din;
 input mem1_ena, clk, rst;
 output logic [15:0] mem1_din, dout;
 
-always @(posedge clk or negedge rst) 
+always_ff @(posedge clk or negedge rst) 
+	if (!rst)
+		begin
+		mem1_din <= 0;
+		dout <= 0;
+		end
+	else if ( mem1_ena == 1'b1)
+		begin
+		mem1_din <= din;
+		dout <= mem1_dout;
+		end
 
-begin
-if (!rst)
-		mem1_din <= 0
-		dout <= 0
-else
-	if ( mem1_ena == 1'b1)
-		mem1_din <= din
-		dout <= mem1_dout
-//	else 
-//		mem1_din = mem1_din
-//		dout = dout
-end
 
 endmodule
